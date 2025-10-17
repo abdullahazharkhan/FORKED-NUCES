519d81ba62b3b6a1346afb38b85405c845a4b7ab
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2025-10-17 21:05:46 +0500

    add django rest framework

--- server/drf_backend/urls.py
+++ server/drf_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for server_core project.
+URL configuration for drf_backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
