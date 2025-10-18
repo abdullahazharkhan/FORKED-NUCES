a29e71cfe0d5c813c6b8a9b3e99fcb1fffff7cab
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2025-10-18 20:16:14 +0500

    adding shadcn

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,13 @@
+// import { Highlighter } from "@/components/ui/highlighter"
+
 export default function Home() {
   return (
-    <div className="bg-primaryblue min-h-screen w-full">
+    <div className="bg-[#6F43FE] min-h-screen w-full flex items-center justify-center flex-col relative space-y-12">
+      <div className="flex flex-col">
+        <h1 className="font-jaro bold text-9xl text-white">FORKED NUCES</h1>
+        <h3 className="text-2xl text-white text-center">FASTians Build Better Together.</h3>
+      </div>
+      <button className="bg-primarygreen text-black p-3 uppercase italic rounded-xl font-bold text-4xl tracking-tighter cursor-pointer">Start Contributing Now!</button>
     </div>
   );
 }
